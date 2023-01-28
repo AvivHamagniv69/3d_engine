4ce9ad03d4a5cc7b7007ec850476a58ac6349868
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2023-01-28 17:01:41 +0200

    Update 3d_engine.c

--- 3d_engine.c
+++ 3d_engine.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
 #define clear() printf("\033[H\033[J")
 #define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
@@ -10,8 +11,12 @@ struct def3DObj {
     char lookPoints;
     char lookEdges;
 
-    int* arrPoints;
-    int* arrEdges;
+    int sizePoint1;
+    int sizePoint2;
+    int **points;
+
+    int sizeEdge;
+    int *edges;
 };
 
 void printObj(int arrPoints[][2], int sizeOfArrPoints, int* arrEdges[][2][2], int sizeOfArrEdges, char lookPoints, char lookEdges) {
@@ -37,14 +42,51 @@ void printObj(int arrPoints[][2], int sizeOfArrPoints, int* arrEdges[][2][2], in
     }
 }
 
+void printPoints(struct def3DObj *ex) {
+    for (int printPoints = 0; printPoints < ex->sizePoint1; printPoints++) {
+        gotoxy(ex->points[printPoints][0], ex->points[printPoints][1]);
+        printf("%c", ex->lookPoints);
+    }
+}
+
+void createPoints(struct def3DObj *ex, ...) {
+    va_list args;
+    va_start(args, ex);
+
+    ex->points = (int**) malloc(sizeof(int*) * ex->sizePoint1);
+    for (int allcArr1 = 0; allcArr1 < ex->sizePoint1; allcArr1++) {
+        ex->points[allcArr1] = (int*) malloc(sizeof(int) * ex->sizePoint2);
+
+        for (int allcArr2 = 0; allcArr2 < ex->sizePoint2; allcArr2++) {
+            ex->points[allcArr1][allcArr2] = va_arg(args, int);
+        }
+    }
+
+    va_end(args);
+}
+
+void createEdges(struct def3DObj *ex, ...) {
+    va_list args;
+    va_start(args, ex);
+
+    ex->edges = (int*) malloc(sizeof(int) * ex->sizeEdge);
+    for (int allcArr = 0; allcArr < ex->sizeEdge; allcArr++) {
+        ex->edges[allcArr] = 
+    }
+}
+
 int main() {
     clear();
+    struct def3DObj sqr;
+    sqr.sizePoint1 = 3;
+    sqr.sizePoint2 = 2;
+    sqr.lookPoints = '*';
 
-    struct def3DObj square;
-    square.arrPoints = malloc(sizeof(int[1][1]));
-    square.arrEdges = malloc(sizeof(int[1][1][1]));
+    createPoints(&sqr, 10, 10, 10, 20, 20, 20);
 
-    free(square.arrPoints);
-    free(square.arrEdges);
+    printPoints(&sqr);
+
+
+    free(sqr.points);
     return 0;
 }
