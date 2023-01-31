8d890d0c0bd8763afcb05fd4733b4fad6ee4cce5
AvivHamagniv <51734410+AvivHamagniv69@users.noreply.github.com>
2023-01-31 15:55:57 +0200

    Update 3d_engine.c

--- 3d_engine.c
+++ 3d_engine.c
@@ -3,27 +3,31 @@
 #include <string.h>
 #include <stdarg.h>
 #include <signal.h>
+#include <math.h>
 
 #define clear() printf("\033[H\033[J")
 #define gotoxy(x,y) printf("\033[%d;%dH", (y), (x))
+
+// TODO: implement FOV.
 
 struct def3DObj {
     int isUsed;
     char lookPoints;
     char lookEdges;
 
-    int sizePoint1;
-    int **points;
+    int sizePoint;
+    int **xyPoints;
+    int **scaledPoints;
 
     int sizeEdge;
     int ***edges;
 };
 
 void printArrsForDebugging(struct def3DObj *ex) {
-    for (int i = 0; i < ex->sizePoint1; i++) {
+    for (int i = 0; i < ex->sizePoint; i++) {
         printf("we are at array %d: \n", i);
-        printf("point x is %d: \n", ex->points[i][0]);
-        printf("point y is %d: \n", ex->points[i][1]);
+        printf("point x is %d: \n", ex->xyPoints[i][0]);
+        printf("point y is %d: \n", ex->xyPoints[i][1]);
     }
 
     for (int i = 0; i < ex->sizeEdge; i++) {
@@ -36,15 +40,17 @@ void printArrsForDebugging(struct def3DObj *ex) {
 }
 
 void printPoints(struct def3DObj *ex) {
-    for (int printPoints = 0; printPoints < ex->sizePoint1; printPoints++) {
-        gotoxy(ex->points[printPoints][0], ex->points[printPoints][1]);
+    for (int printPoints = 0; printPoints < ex->sizePoint; printPoints++) {
+        gotoxy(ex->xyPoints[printPoints][0], ex->xyPoints[printPoints][1]);
         printf("%c", ex->lookPoints);
     }
 }
 
 void printEdge(struct def3DObj *ex, int eachEdge, int primaryAxis, int secondaryAxis, int howMuchPrimaryAxis, int howMuchSecondaryAxis, int whatIsPrimary) {
     while (primaryAxis < howMuchPrimaryAxis || primaryAxis > howMuchPrimaryAxis) {
-        if (whatIsPrimary == 0) {
+        const short int Y_ISNT_ZERO = 0;
+
+        if (whatIsPrimary == Y_ISNT_ZERO) {
             gotoxy(ex->edges[eachEdge][0][0]+secondaryAxis, ex->edges[eachEdge][0][1]+primaryAxis);
         } else {
             gotoxy(ex->edges[eachEdge][0][0]+primaryAxis, ex->edges[eachEdge][0][1]+secondaryAxis);
@@ -78,20 +84,29 @@ void printEdges(struct def3DObj *ex) {
     }
 }
 
+void createScaledPoints(struct def3DObj *ex) {
+
+}
+
 void createPoints(struct def3DObj *ex, ...) {
     va_list args;
     va_start(args, ex);
 
-    ex->points = (int**) malloc(sizeof(int*) * ex->sizePoint1);
-    for (int allcArr1 = 0; allcArr1 < ex->sizePoint1; allcArr1++) {
-        ex->points[allcArr1] = (int*) malloc(sizeof(int) * 2);
+    const short int AMT_OF_DIMENTIONS = 3;
+    ex->xyPoints = (int**) malloc(sizeof(int*) * ex->sizePoint);
+    for (int allcArr1 = 0; allcArr1 < ex->sizePoint; allcArr1++) {
+        ex->xyPoints[allcArr1] = (int*) malloc(sizeof(int) * AMT_OF_DIMENTIONS);
 
-        for (int allcArr2 = 0; allcArr2 < 2; allcArr2++) {
-            ex->points[allcArr1][allcArr2] = va_arg(args, int);
+        for (int allcArr2 = 0; allcArr2 < AMT_OF_DIMENTIONS; allcArr2++) {
+            ex->xyPoints[allcArr1][allcArr2] = va_arg(args, int);
         }
     }
-
     va_end(args);
+
+    for (int allcArr1 = 0; allcArr1 < ex->sizePoint; allcArr1++) {
+        ex->xyPoints[allcArr1][0] /= ex->xyPoints[allcArr1][2];
+        ex->xyPoints[allcArr1][1] /= ex->xyPoints[allcArr1][2];
+    }
 }
 
 void createEdges(struct def3DObj *ex, ...) {
@@ -103,19 +118,18 @@ void createEdges(struct def3DObj *ex, ...) {
         ex->edges[allEdges] = (int**) malloc(sizeof(int*) * 2);
 
         for (int pointers = 0; pointers < 2; pointers++) {
-            int* pointer = ex->points[va_arg(args, int)];
+            int* pointer = ex->xyPoints[va_arg(args, int)];
             ex->edges[allEdges][pointers] = pointer;
         }
     }
-
     va_end(args);
 }
 
 void freePoints(struct def3DObj *ex) {
-    for (int freeP = 0; freeP < ex->sizePoint1; freeP++) {
-        free(ex->points[freeP]);
+    for (int freeP = 0; freeP < ex->sizePoint; freeP++) {
+        free(ex->xyPoints[freeP]);
     }
-    free(ex->points);
+    free(ex->xyPoints);
 }
 
 void freeEdges(struct def3DObj *ex) {
@@ -128,14 +142,14 @@ void freeEdges(struct def3DObj *ex) {
 int main() {
     clear();
     struct def3DObj sqr;
-    sqr.sizePoint1 = 3;
+    sqr.sizePoint = 8;
     sqr.lookPoints = '*';
 
-    sqr.sizeEdge = 3;
+    sqr.sizeEdge = 1;
     sqr.lookEdges = '*';
 
-    createPoints(&sqr, 10, 10, 10, 20, 20, 20);
-    createEdges(&sqr, 0, 1, 1, 2, 2, 0);
+    createPoints(&sqr, 10, 20, 1, 10, 20, 2, 20, 20, 2, 20, 20, 1, 10, 10, 2, 10, 10, 1, 20, 10, 1, 20, 10, 2);
+    createEdges(&sqr, 0, 1);
 
     //printArrsForDebugging(&sqr);
     printPoints(&sqr);
